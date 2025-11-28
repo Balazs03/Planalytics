2ad063ce44ddf51ae347539f7e5b6f0bb6a790de
Balazs03 <balazsszabo003@gmail.com>
2025-11-28 16:01:39 +0100

    projekt létrehozása

---
+++ Planalytics/Planalytics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planalytics
+//
+//  Created by Szabó Balázs on 2025. 11. 28..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planalytics/Planalytics/PlanalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanalyticsApp.swift
+//  Planalytics
+//
+//  Created by Szabó Balázs on 2025. 11. 28..
+//
+
+import SwiftUI
+
+@main
+struct PlanalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
